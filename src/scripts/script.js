7d31cb4e039cb43f3487a482c9a411d1f1b5a10f
Angad Trehan <angadtrehan@HDT022200234.local>
2025-03-17 02:01:43 +0530

    Change some small stuff and play around with the mouse

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,6 +6,14 @@ import gsap from "gsap";
 console.log("Ninki");
 
 MouseFollower.registerGSAP(gsap);
+const mouse = document.querySelector("#cursor");
+const bText = document.querySelector("#b");
+const wText = document.querySelector("#W");
 
-const cursor = new MouseFollower();
+const cursor = new MouseFollower({
+    el: mouse,
+    visible: true,
+    speed: 0.5,
+
+});
 cursor.show();
